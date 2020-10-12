efb5fd990480ad60fce0beff546068da9df958ac
Arisodee <arisodee@gmail.com>
2020-10-12 11:32:31 +0300

    Add an on click function to display descriptions about what we do

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,9 @@
 $(".portfolio-card").hover(function(){
     $(this).children(".portfolio-card-content").fadeToggle()
 })
+
+//on click what we do section is 
+
+$(".what-we-do").click(function(){
+    $(this).children(".we-do-content").slideToggle(1000)
+})
