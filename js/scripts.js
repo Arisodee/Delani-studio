7adc753a7c7dbd56c4d275fca9c11fd0a94507ba
Arisodee <arisodee@gmail.com>
2020-10-12 12:50:23 +0300

    Add functions to validate the details entered in contact us section and display a message to the user once submitted

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,22 @@ $(".portfolio-card").hover(function(){
 $(".what-we-do").click(function(){
     $(this).children(".we-do-content").slideToggle(1000)
 })
+
+function replyToCustomer(){
+    // validate details entered
+
+    let nameInput = document.getElementById("fullnames")
+    if(nameInput.value === ""){
+        alert("name is required")
+        return
+    }
+
+    let emailInput = document.getElementById("email")
+
+    if(emailInput.value === ""){
+        alert("Please enter your email address")
+        return
+    }
+
+    alert(nameInput.value +" we have received your message. Thank you for reaching out to us")
+}
